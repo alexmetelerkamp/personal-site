d57ed1d94a8d4be96d18bf481b20615aa8240532
alexmetelerkamp <47564173+alexmetelerkamp@users.noreply.github.com>
2020-11-02 15:32:28 +1100

    add requirements and runtime

--- myapp.py
+++ myapp.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('home.html')
-
+    return render_template("home.html")
+    #return "hello world"
 
 if __name__ == '__main__':
     app.run(debug=True)
